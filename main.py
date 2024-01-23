12757ffd14c1139b7ceedcf508d4183fae9b02b6
Merrkoll <narekv653@gmail.com>
2024-01-23 13:42:31 +0700

    Class Spec

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ class Student:
 
     def __str__(self):
         return
+
+
+class Spec:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        return
+
+    def __repr__(self):
+        return
